de82f781f0adfd3acb455c45b4935e33cf7b6d3a
parag goyal <paragg1998@gmail.com>
2025-05-08 06:52:18 +0530

    CREATE update_orderbook update_tradebook with tests respectively

---
+++ frontend/src/lib/tradebook.ts
@@ -0,0 +1,28 @@
+
+import { Side } from "./orderbook"
+import data from "@common/config.json"
+
+
+export interface Trade{
+    price: number,
+    quantity: number,
+    timestamp: number,
+    side: Side
+}
+
+export interface TradeBook{
+    maxTradeBookSize: number,
+    trades: Array<Trade>
+}
+
+export function insertIntoTradebook(tradebook: TradeBook, trade: Trade){
+    tradebook.trades.push(trade);
+    if(tradebook.trades.length>tradebook.maxTradeBookSize){
+        tradebook.trades = tradebook.trades.filter((_, i)=>{
+            return i!==0;
+        });
+    }
+}
+
+
+
