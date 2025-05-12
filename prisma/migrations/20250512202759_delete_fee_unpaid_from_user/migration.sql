7665604884bcd46f17d52b0e6248f906ebe80ca0
parag goyal <paragg1998@gmail.com>
2025-05-13 01:58:46 +0530

    removed unpaid fee from user

---
+++ prisma/migrations/20250512202759_delete_fee_unpaid_from_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `fee_unpaid` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "fee_unpaid";
