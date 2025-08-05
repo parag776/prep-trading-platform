b33c657bd65cd0686eb7def115640845f3f9ab21
parag goyal <paragg1998@gmail.com>
2025-08-06 02:18:42 +0530

    created order cancellation job

---
+++ backend/src/jobs/cancelledOrderCleanupJob.ts
@@ -0,0 +1,37 @@
+import configData from "../../../shared/config.mjs";
+import prisma from "../database.js";
+import { getAllUsers, getDetailedUsersState } from "src/store/userStore.js";
+
+export function initializeOrderCancellationJob() {
+	const maxOrders = configData.order_history_size;
+	setInterval(async () => {
+		for (const [userId, user] of getAllUsers()) {
+            
+			const ordersToKeep = (
+				await prisma.order.findMany({
+					where: {
+						userId,
+						status: "CANCELLED",
+					},
+					orderBy: {
+						updatedAt: "desc",
+					},
+					take: maxOrders,
+					select: {
+						id: true,
+					},
+				})
+			).map((order) => order.id);
+
+			await prisma.order.deleteMany({
+				where: {
+					userId,
+					status: "CANCELLED",
+					id: {
+						notIn: ordersToKeep,
+					},
+				},
+			});
+		}
+	}, 60 * 60 * 1000); // every hour
+}
